4b0ec82fccd6e3ceb387c25c03cfcf16b7d4a434
rajat gurung <rjtgurung@gmail.com>
2021-02-22 11:22:14 +0530

    resolved ormconfig and nodemon

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { join } = require("path");
 
 module.exports = {
   name: "default",
   type: "postgres",
-  host: "localhost",
+  host: process.env.DB_HOST,
   port: 5432,
-  username: "hunter",
-  password: "root",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   logging: true,
-  database: "project_hunt",
+  database: process.env.DB_SCHEMA,
   entities: [join(__dirname, "dist/entities", "*.{ts,js}")],
   migrations: [__dirname + "/src/migrations/*.{ts,js}"],
   subscribers: [__dirname + "/src/subscribers/*.{ts,js}"]
