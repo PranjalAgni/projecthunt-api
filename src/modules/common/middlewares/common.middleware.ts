bfde269ae42e6a201f50a8527d564ced856f9252
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-03-25 23:37:13 +0530

    :hammer: fix: middleware issue

--- src/modules/common/middlewares/common.middleware.ts
+++ src/modules/common/middlewares/common.middleware.ts
@@ -1,3 +1,8 @@
+import config from "@config/index";
+import { NextFunction, Request, Response } from "express";
+import createError from "http-errors";
+import { StatusCodes } from "http-status-codes";
+
 // const debugLog = debug("server:common-middleware");
 class CommonMiddleware {
   private static instance: CommonMiddleware;
@@ -7,6 +12,29 @@ class CommonMiddleware {
       CommonMiddleware.instance = new CommonMiddleware();
     }
     return CommonMiddleware.instance;
+  }
+
+  notFound(req: Request, res: Response, next: NextFunction) {
+    res.status(StatusCodes.NOT_FOUND);
+    return next(
+      createError(StatusCodes.NOT_FOUND, `${req.originalUrl} not found`)
+    );
+  }
+
+  errorHandler(
+    error: Error,
+    _req: Request,
+    res: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _next: NextFunction
+  ) {
+    const statusCode = res.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+    res.status(statusCode);
+    return res.json({
+      status: statusCode,
+      result: null,
+      error: config.isDev ? error.stack : []
+    });
   }
 }
 
